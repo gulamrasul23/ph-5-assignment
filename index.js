564f73efdec5f38019aaf6d71878537524c7e737
Gulam Rasul <gulamrasulrahim23@gmail.com>
2025-08-28 00:20:04 +0600

    dom

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+
+let callHistoryData = [];
+
+
+
 document.getElementById('card-container')
     .addEventListener('click', function(e){
         if(e.target.classList.contains('heart')){
@@ -5,4 +10,64 @@ document.getElementById('card-container')
             const newHeartCount = heartCount + 1;
             document.getElementById("heart-count").innerText = newHeartCount;
         }
+
+        if(e.target.classList.contains('call-button')){
+            const card = e.target.closest('.card');
+            const title = card.querySelector('.title-name').textContent;
+            const number = card.querySelector('.call-number').textContent;
+            const availableCoin = parseInt(document.getElementById('available-coin').innerText);
+            if(availableCoin < 20){
+                alert("❌ You don't have enough coins, you need at least 20 coins to make a call.");
+                return;
+            }else {
+                alert("📞 Calling" + " " + title + " " + number);
+                const callHistory = {
+                    name: title,
+                    callNumber: number,
+                    date: new Date().toLocaleTimeString()
+                }
+                callHistoryData.push(callHistory);
+
+                const callHistoryParent = document.getElementById('call-history');
+                    callHistoryParent.innerText = '';
+                for(const history of callHistoryData){
+                    const div = document.createElement("div");
+                    div.innerHTML = `
+                        <div  class="flex justify-between items-center gap-2 bg-[#FAFAFA] p-[12px] rounded-[10px] mt-2">
+                        <div class=" w-[62%]">
+                            <h2 class="font-semibold text-[18px] leading-5">${history.name}</h2>
+                            <p class="text-[#5C5C5C]">${history.callNumber}</p>
+                        </div>
+                        <div>
+                            <span class="text-ms">${history.date}</span>
+                        </div>
+                    </div>
+                    
+                    `
+                    callHistoryParent.appendChild(div);
+                }
+
+            }
+            const newAvailableCoin = availableCoin - 20;
+            document.getElementById('available-coin').innerText = newAvailableCoin;
+        }
+
+        if(e.target.classList.contains('copy-button')){
+            const card = e.target.closest('.card');
+            const number = card.querySelector('.call-number').textContent;
+            
+            navigator.clipboard.writeText(number);
+            alert("Copied:" + " " + number);
+        
+        }
+
+        
     })
+
+
+    document.getElementById('clear-button')
+                .addEventListener('click', function(e){
+                    const callHistoryParent = document.getElementById('call-history');
+                    callHistoryParent.innerText = '';
+                    callHistoryData = [];
+                })
